e5fca32ff6aef7f4746f145c695c41de55b6689a
patszkarlat <punkgeek@mac.com>
2014-06-04 16:23:30 -0400

    another commit

--- exercise6.rb
+++ exercise6.rb
@@ -15,3 +15,29 @@ puts "\nUpdated Grocery List\n\n"
 list(grocery_list)
 
 puts "\n" + grocery_list.count.to_s
+
+puts "\n"
+
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas!"
+else
+  puts "You do not need to pick up bananas today!" 
+end
+
+def blank_line
+	puts
+end
+
+blank_line
+
+puts grocery_list[1] 
+
+blank_line
+
+list(grocery_list.sort)
+
+grocery_list.delete('salami')
+
+blank_line
+
+list(grocery_list)
